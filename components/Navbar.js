ac4e9155c91afc3a16537a4fd28af9fd9d5d4baf
Tiago Marques <tiagomarques@Tiagos-MacBook-Pro.local>
2024-02-04 17:57:43 -1000

    add components folder with navbar, and layout changes

---
+++ components/Navbar.js
@@ -0,0 +1,52 @@
+'use client'
+
+import Link from 'next/link'
+import { signOut, useSession } from 'next-auth/react'
+
+
+function Navbar() {
+  const{ data: session } = useSession()
+
+  return (
+    <div className="bg-black fixed p-4">
+      <div className="container mx-auto">
+        <ul className="flex justify-between h-screen flex-col">
+          
+          <div className="one">
+            <li className="mx-4 mt-5">
+                <Link href="/" className="text-white font-bold">
+                  Home
+                </Link>
+            </li>
+            <li className="mx-4 mt-5">
+                <Link href="/dashboard" className="text-white font-bold">
+                Dashboard
+                </Link>
+            </li>
+          </div>
+
+          { /* ----------------------- */}
+
+          <div className="auth">
+            <>
+              <li className="mx-4 mt-[2rem]">
+                  <Link href="/login" className="text-white font-bold">
+                    Login
+                  </Link>
+              </li>
+              <li className="mx-4 mb-5">
+                  <Link href="/register" className="text-white font-bold">
+                  Register
+                  </Link>
+              </li>
+            </>
+          </div>
+
+
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
